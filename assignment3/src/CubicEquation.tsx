e1dcfe5028e4d0761508e3063ab2dae70e730cfc
unknown <342896818@TDSB.ON.CA>
2026-03-25 10:54:15 -0400

    IDK

---
+++ assignment3/src/CubicEquation.tsx
@@ -0,0 +1,15 @@
+import { useState, useRef } from "react";
+
+export const CubicEquation = () => {
+    const [a, setA] = useState<number>(0);
+    const [b, setB] = useState<number>(0);
+    const [c, setC] = useState<number>(0);
+    const [d, setD] = useState<number>(0);
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const handleSubmit
+
+    return (
+
+    )
+}
