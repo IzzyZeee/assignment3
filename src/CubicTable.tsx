1c09cd1cf081546882da8b64651e2fe8d13d0321
Isadora Zhang <izzyorsomething@gmail.com>
2026-03-29 16:31:27 -0400

    turned all the state variables to string so the truncate issue is gone. but need to fix floating point error

--- src/CubicTable.tsx
+++ src/CubicTable.tsx
@@ -5,13 +5,13 @@ export default function CubicEquation
 
 ({ coefficients, update }: UpdateCoefficient) {
     
-    const [p_val, setP] = useState<number>(0); // States include p, q, delta, x1/2/3
-    const [q_val, setQ] = useState<number>(0);
-    const [delta_val, setDelta] = useState<number>(0);
-    const [x1, setX1] = useState<number>(0);
-    const [x2, setX2] = useState<number>(0);
-    const [x3, setX3] = useState<number>(0);
-    const [d_val, setD] = useState<number>(0); // y-intercept
+    const [p_val, setP] = useState<string>(""); // States include p, q, delta, x1/2/3
+    const [q_val, setQ] = useState<string>(""); // I made them String because number will chop off useless zeros
+    const [delta_val, setDelta] = useState<string>("");
+    const [x1, setX1] = useState<string>("");
+    const [x2, setX2] = useState<string>("");
+    const [x3, setX3] = useState<string>("");
+    const [d_val, setD] = useState<string>(""); // y-intercept
 
     useEffect (() => {
     
@@ -19,24 +19,28 @@ export default function CubicEquation
         const b = coefficients.b;
         const c = coefficients.c;
         const d = coefficients.d;
-              
+
+        if (a == 0) { // a-value cannot be zero. Does nothing until valid a-value is put
+            return;
+        }
+        
         const p = (3 * a * c - b * b) / (3 * a * a);
         const q = (27 * a * a * d - 9 * a * b * c + 2 * b * b * b) / (27 * a * a * a);
         const delta = Math.pow(q / 2, 2) + Math.pow(p / 3, 3);
 
-        setP(Number(p.toFixed(5))); // must cast to Number. toFixed fixes it to 5 places (truncate function not needed)
-        setQ(Number(q.toFixed(5)));
-        setDelta(Number(delta.toFixed(5)));
-        setD(Number(d.toFixed(2)));
+        setP(p.toFixed(5)); // It's a string. toFixed fixes it to 5 places (truncate function not needed)
+        setQ(q.toFixed(5));
+        setDelta(delta.toFixed(5));
+        setD(d.toFixed(2));
 
         function cardano(a: number, b: number, p: number, q: number) { // Calculates a single root
-            return Number((Math.cbrt(-q / 2 + Math.sqrt(Math.pow(q / 2, 2) + Math.pow(p / 3, 3))) + Math.cbrt(-q / 2 - Math.sqrt(Math.pow(q / 2, 2) + Math.pow(p / 3, 3))) - b / (3 * a)).toFixed(2)); // Truncated to 2 decimal places like on the example
+            return String((Math.cbrt(-q / 2 + Math.sqrt(Math.pow(q / 2, 2) + Math.pow(p / 3, 3))) + Math.cbrt(-q / 2 - Math.sqrt(Math.pow(q / 2, 2) + Math.pow(p / 3, 3))) - b / (3 * a)).toFixed(2)); // Truncated to 2 decimal places like on the example
             // return Math.cbrt(-q / 2 + Math.sqrt(Math.pow(q / 2, 2) + Math.pow(p / 3, 3))) + Math.cbrt(-q / 2 - Math.sqrt(Math.pow(q / 2, 2) + Math.pow(p / 3, 3))) - b / (3 * a); // Non truncated version
         }
 
         if (Math.abs(delta) < 1e-15) { // Case C: Delta equals 0, but sometimes the computer can't actually get the zero so it becomes very close to zero, which we detect under the threshold (between 0 and 1e-15)
 
-            setDelta(0); // Turns delta to zero
+            setDelta("0.00000"); // Turns delta to zero
 
             if (Math.abs(p) < 1e-15 && Math.abs(q) < 1e-15) { // Case C1: Triple root when p = q = 0
             
@@ -47,24 +51,23 @@ export default function CubicEquation
             } else { // Case C2: Double root
 
                 setX1(cardano(a, b, p, q)); // Double
-                setX2(Number((Math.cbrt(q / 2) - b / (3 * a)).toFixed(2))); // Single
-                // setX3(x2); // Same as x2
-                setX3(Number((Math.cbrt(q / 2) - b / (3 * a)).toFixed(2)));
+                setX2((Math.cbrt(q / 2) - b / (3 * a)).toFixed(2)); // Single
+                setX3((Math.cbrt(q / 2) - b / (3 * a)).toFixed(2));
 
             }
 
         } else if (delta < 0) { // Case A: 3 real roots
 
             const theta = (1 / 3) * Math.acos(-q / (2 * Math.sqrt(-Math.pow(p / 3, 3))));
-            setX1(Number((2 * Math.sqrt(-p / 3) * Math.cos(theta) - b / (3 * a)).toFixed(2)));
-            setX2(Number((2 * Math.sqrt(-p / 3) * Math.cos(theta + (2 * Math.PI) / 3) - b / (3 * a)).toFixed(2)));
-            setX3(Number((2 * Math.sqrt(-p / 3) * Math.cos(theta + (4 * Math.PI) / 3) - b / (3 * a)).toFixed(2)));
+            setX1((2 * Math.sqrt(-p / 3) * Math.cos(theta) - b / (3 * a)).toFixed(2));
+            setX2((2 * Math.sqrt(-p / 3) * Math.cos(theta + (2 * Math.PI) / 3) - b / (3 * a)).toFixed(2));
+            setX3((2 * Math.sqrt(-p / 3) * Math.cos(theta + (4 * Math.PI) / 3) - b / (3 * a)).toFixed(2));
 
         } else { // Case B: Delta > 0, 1 real root and 2 complex roots
 
             setX1(cardano(a, b, p, q));
-            setX2(NaN);
-            setX3(NaN);
+            setX2("Complex Number");
+            setX3("Complex Number");
 
         }
             
